8db78da8e8b38eee7e35c9c9e7f7cbad99120fdc
GusWard <guss.warrd@googlemail.com>
2021-06-10 11:39:22 +0100

    moved jacoco setup into jacoco.gradle.kts removed ios & android specific tests set rules for branch, method & class

--- HeaderTestModule/src/commonTest/kotlin/com/unidays/headertestmodule/commonTest.kt
+++ HeaderTestModule/src/commonTest/kotlin/com/unidays/headertestmodule/commonTest.kt
@@ -1,5 +1,6 @@
 package com.unidays.headertestmodule
 
+import com.unidays.headertestmodule.HeaderBuilder.Companion.SOME_OTHER_HEADER
 import com.unidays.headertestmodule.HeaderBuilder.Companion.TEST_HEADER
 import com.unidays.headertestmodule.HeaderBuilder.Companion.TEST_HEADER_ID
 import kotlin.test.Test
@@ -8,7 +9,11 @@ import kotlin.test.assertTrue
 
 class CommonHeaderBuilderTest {
     @Test
-    fun testExample() {
+    fun testExampleOne() {
         assertEquals(HeaderBuilder().getHeader(TEST_HEADER_ID), TEST_HEADER)
+    }
+    @Test
+    fun testExampleTwo() {
+        assertEquals(HeaderBuilder().getHeader(TEST_HEADER_ID + 1), SOME_OTHER_HEADER)
     }
 }
