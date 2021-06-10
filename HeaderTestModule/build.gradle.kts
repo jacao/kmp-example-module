f7cbf9c3888003c116b6680bc29ac1e8f012cde7
GusWard <guss.warrd@googlemail.com>
2021-06-10 09:03:09 +0100

    added scripts for jacoco

--- HeaderTestModule/build.gradle.kts
+++ HeaderTestModule/build.gradle.kts
@@ -5,6 +5,11 @@ plugins {
     id("com.android.library")
     id("com.chromaticnoise.multiplatform-swiftpackage") version "2.0.3"
     id("kotlin-android-extensions")
+    jacoco
+}
+
+jacoco {
+    toolVersion = "0.8.5"
 }
 
 kotlin {
@@ -58,4 +63,48 @@ multiplatformSwiftPackage {
     targetPlatforms {
         iOS { v("13") }
     }
+}
+
+val jacocoTestReport by tasks.creating(JacocoReport::class.java) {
+    dependsOn("test")
+    reports {
+        xml.isEnabled = true
+        csv.isEnabled = true
+        html.isEnabled = true
+        File("${buildDir}/jacocoHtml")
+    }
+    classDirectories.setFrom(fileTree("${project.buildDir}/tmp/kotlin-classes/debug"))
+    sourceDirectories.setFrom(files(project.projectDir))
+    executionData.setFrom(
+        fileTree(project.projectDir) {
+            setIncludes(setOf("build/jacoco/*.exec"))
+        }
+    )
+}
+
+val jacocoTestCoverageVerification by tasks.creating(JacocoCoverageVerification::class.java) {
+    dependsOn(jacocoTestReport)
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.95".toBigDecimal()
+            }
+        }
+    }
+    classDirectories.setFrom(fileTree("${project.buildDir}/tmp/kotlin-classes/debug"))
+    sourceDirectories.setFrom(files(project.projectDir))
+    executionData.setFrom(
+        fileTree(project.projectDir) {
+            setIncludes(setOf("build/jacoco/*.exec"))
+        }
+    )
+}
+
+val testCoverage by tasks.registering {
+    group = "verification"
+    description = "Runs the unit tests with coverage."
+
+    dependsOn("test", jacocoTestReport, jacocoTestCoverageVerification)
+    tasks["jacocoTestReport"].mustRunAfter("test")
+    tasks["jacocoTestCoverageVerification"].mustRunAfter("jacocoTestReport")
 }
