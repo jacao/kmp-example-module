0e4edeace5c6f8cdfff950b58c01e837a7b8c4ca
Jacob Hughes <jacob.hughes@myunidays.com>
2021-06-24 11:53:56 +0100

    Add extra greeting code

--- KMPExampleModule/src/commonTest/kotlin/com/example/kmpexamplemodule/commonTest.kt
+++ KMPExampleModule/src/commonTest/kotlin/com/example/kmpexamplemodule/commonTest.kt
@@ -6,7 +6,13 @@ import kotlin.test.assertTrue
 class CommonGreetingTest {
 
     @Test
-    fun testExample() {
+    fun testGreeting() {
         assertTrue(Greeting().greeting().contains("Hello"), "Check 'Hello' is mentioned")
+    }
+
+    @Test
+    fun testExtraGreeting() {
+        assertTrue(Greeting().extraGreeting().contains("Hello"), "Check 'Hello' is mentioned")
+        assertTrue(Greeting().extraGreeting().contains("friend"), "Check 'friend' is mentioned")
     }
 }
