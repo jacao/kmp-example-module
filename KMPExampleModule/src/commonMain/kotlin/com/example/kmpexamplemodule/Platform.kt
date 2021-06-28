80e33fe746e9088f6fcb402edc5f68b1e288e8e8
GusWard <guss.warrd@googlemail.com>
2021-06-28 15:49:23 +0100

    Changed detekt version to 1.15.0 everything working again

--- KMPExampleModule/src/commonMain/kotlin/com/example/kmpexamplemodule/Platform.kt
+++ KMPExampleModule/src/commonMain/kotlin/com/example/kmpexamplemodule/Platform.kt
@@ -1,5 +1,8 @@
 package com.example.kmpexamplemodule
 
+/* we need to suppress this as the expect / actual keywords do not work without an empty constructor.
+*  those key words are still in beta so this should fix itself eventually */
+@Suppress("EmptyDefaultConstructor")
 expect class Platform() {
-    val platform: String
+    val name: String
 }
